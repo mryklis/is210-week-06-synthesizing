5b34a812086d8e23b5478f1a11ba2438bbaea7f3
Maxim Ryklis <maxim.ryklis@spsmail.cuny.edu>
2016-10-01 18:57:37 +0000

    doc strings

--- task_01.py
+++ task_01.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""THis module counts guests and number of tables needed"""
 
 
 def get_party_stats(families, table_size=6):
+    """choose number of tables based on guests and maximum table size
+
+    Args:
+        families (list): number of families and guests per family
+        table_size (int): maximum number of guests per table
+
+    Returns:
+        guests (int): total number of guests
+        tables (int): number of tables based on contraints
+
+    Examples:
+        >>> get_party_stats(['Jan'], ['Jen', 'Jess'], ['Jem', 'Jack', 'Janis']])
+        (6, 3)
+
+        >>> get_party_stats(['Jan'], ['Jen', 'Jess'], ['Jem', 'Jack', 'Janis']], 2)
+        (6, 4)
+    """
+
     tables = 0
     guests = 0
     for obj in families:
--- task_02.py
+++ task_02.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+"""This module formats a string with given inputs"""
 
 def prepare_email(appointments):
+    """Takes a string and formats with given inputs
+
+    Args:
+        appointments (list): name and date tuple
+
+    Returns:
+        email (str): format_string with given inputs
+
+    Example:
+        >>> prepare_email([('Jen', '2015'), ('Max', 'March 3')]
+        ['Dear Jen,\nI look forward to meeting with you on 2015.\nBest,\nMe',
+        'Dear Max,\nI look forward to meeting you on March 3.\nBest\nMe']
+
+    """
+
     format_string = 'Dear {},\nI look forward to meeting with you on {}.\nBest,\nMe'
     for obj in appointments:
         email = format_string.format(*appointments)
